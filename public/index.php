3151a5007e5092cfc6d1513d5ae7da8caf08da18
wuwenbin <wenbin.wu@foxmail.com>
2014-11-19 13:32:06 +0800

    修改配置文件名

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ define("APP_PATH", dirname(__DIR__)."/app");
 
 require __DIR__."/../vendor/autoload.php";
 
-Flight::set(require APP_PATH."/config/global.php");
+Flight::set(require APP_PATH."/config/app.php");
 Flight::path(Flight::get("flight.controllers.path"));
 Flight::path(Flight::get("flight.models.path"));
 Flight::path(Flight::get("flight.libs.path"));
