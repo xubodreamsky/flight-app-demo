913ff7c2f5f678c7bb21e5afc6ad5e90b3c5885c
wuwenbin <wenbin.wu@foxmail.com>
2014-11-25 11:54:32 +0800

    增加公共初始脚本

--- public/index.php
+++ public/index.php
@@ -3,16 +3,7 @@
  * Demo Application
  */
 
-define("START_TIME", microtime());
-define("APP_PATH", dirname(__DIR__)."/app");
-
-require __DIR__."/../vendor/autoload.php";
-
-Flight::set(require APP_PATH."/config/app.php");
-Flight::path(Flight::get("flight.controllers.path"));
-Flight::path(Flight::get("flight.models.path"));
-Flight::path(Flight::get("flight.libs.path"));
-
+require __DIR__."/../bootstrap/init.php";
 Flight::before("start", array("Controller", "init"));
 Flight::start();
 
